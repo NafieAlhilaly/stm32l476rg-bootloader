67656f4909d7508562166ae1296edc2abe69f546
NafieAlhilaly <warrior259@gmail.com>
2025-02-20 00:45:32 +0300

    feat: add bootloader source and header files

--- bootloader/src/bootloader.c
+++ bootloader/src/bootloader.c
@@ -1,3 +1,14 @@
+#include "bootloader.h"
+
+
+void jump_to_firmware(){
+  uint32_t* reset_vector_entry = (uint32_t*)(FIRMWARE_START_ADDRESS + VECTOR_TABLE_ENTRY_LENGTH);
+  uint32_t* reset_vector       = (uint32_t*)(*reset_vector_entry);
+  void_fn jump_fn              = (void_fn)reset_vector;
+  jump_fn();
+}
+
 int main(void){
-    return 0;
+  jump_to_firmware();
+  return 0;
 }
