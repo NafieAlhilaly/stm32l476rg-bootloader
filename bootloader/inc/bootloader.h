c3b949c3287ee91eab72c4cf609ea2bc06a93130
warrior259@gmail.com <warrior259@gmail.com>
2025-03-11 00:20:12 +0300

    refactor(bootloader): restructure reset vector code using vector_table_t

--- bootloader/inc/bootloader.h
+++ bootloader/inc/bootloader.h
@@ -1,10 +1,7 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/cm3/scb.h>
+#include <libopencm3/cm3/vector.h>
 
 
 #define BOOTLOADER_SIZE (0x8000U)
 #define FIRMWARE_START_ADDRESS (FLASH_BASE + BOOTLOADER_SIZE)
-#define VECTOR_TABLE_ENTRY_LENGTH (4U)
-
-typedef void (*void_fn)(void);
-void jump_to_firmware(void);
