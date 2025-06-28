306da1b33d67a1c6fd1294e0c65c8cb8f048cdd4
Nafea Alhelali <warrior259@gmail.com>
2025-06-28 14:08:19 +0300

    chore: import stm32l4's rcc and memorymap

--- bootloader/inc/bootloader.h
+++ bootloader/inc/bootloader.h
@@ -1,6 +1,7 @@
-#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/l4/rcc.h>
 #include <libopencm3/cm3/scb.h>
 #include <libopencm3/cm3/vector.h>
+#include <libopencm3/stm32/l4/memorymap.h>
 
 
 #define BOOTLOADER_SIZE (0x8000U)
